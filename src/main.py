1da841202f664ca56d6409ab8bd268bf5baccaeb
Colby Goettel <colby.goettel@gmail.com>
2026-01-03 13:43:00 -0700

    Resolve "Add mypy type checking"

--- src/main.py
+++ src/main.py
@@ -3,11 +3,7 @@
 import argparse
 import math
 import sys
-import tempfile
-import wave
 
-import cv2
-import numpy as np
 import rasterio
 import rasterio.io
 import rasterio.plot
@@ -66,69 +62,71 @@ def convert_geotiff_to_wt(dataset: rasterio.io.DatasetReader, user_specified_ban
     Returns:
         A list of bytes representing the frames from the WAV file.
     """
-    # Read the data from the specified band and store it.
-    array: np.ndarray = dataset.read(user_specified_band)
-    # wt files support wave cycles of length 2-4096 (as powers of 2) and we can't guarantee that our input data will have a number of waves that's a power of 2. Additionally, we can only have a wave count of 1-512 waves.
-    # We'll need to resize our array so the wave size (determined by len(array[n])) is a power of 2 in the range 2-4096 and the wave count (determined by len(array)) is between 1-512, inclusive.
-    # A little discussion: To illustrate this point, the test input data I'm using (GeoTIFF of Oro Valley, AZ, USA) is 3612x3612. 3612 is between 2048 and 4096 so we have a choice to make: do we resize down and lose data, or do we resize up and maybe incorrectly interpolate data? I've tried both and they're both really accurate. 2048 sounds great in Bitwig. Let's just use the next greatest and be done with it.
-    # TODO: add a flag to control the width. lower resolution could produce a crunchier tone. see issue #4.
-    resize_width: int = calculate_width(dataset.width)
-    resize_height: int = calculate_height(dataset.height)
+    return []  # TODO: implement this function
 
-    # With out new width and height determined, we can resize the ndarray to the new size.
-    # TODO: add a flag to switch interpolation algorithms. more: https://stackoverflow.com/questions/48121916/numpy-resize-rescale-image
-    resized_array = cv2.resize(array, dsize=(resize_width, resize_height), interpolation=cv2.INTER_CUBIC)
-    # If you would like to play around with the dsize dimensions and visualize the output, so that and uncomment this next line:
-    # rasterio.plot.show(resized_array)
+    # # Read the data from the specified band and store it.
+    # array: np.ndarray = dataset.read(user_specified_band)
+    # # wt files support wave cycles of length 2-4096 (as powers of 2) and we can't guarantee that our input data will have a number of waves that's a power of 2. Additionally, we can only have a wave count of 1-512 waves.
+    # # We'll need to resize our array so the wave size (determined by len(array[n])) is a power of 2 in the range 2-4096 and the wave count (determined by len(array)) is between 1-512, inclusive.
+    # # A little discussion: To illustrate this point, the test input data I'm using (GeoTIFF of Oro Valley, AZ, USA) is 3612x3612. 3612 is between 2048 and 4096 so we have a choice to make: do we resize down and lose data, or do we resize up and maybe incorrectly interpolate data? I've tried both and they're both really accurate. 2048 sounds great in Bitwig. Let's just use the next greatest and be done with it.
+    # # TODO: add a flag to control the width. lower resolution could produce a crunchier tone. see issue #4.
+    # resize_width: int = calculate_width(dataset.width)
+    # resize_height: int = calculate_height(dataset.height)
 
-    # There are two approaches I can think of:
-    # 1. convert ndarray -> WAV and then WAV -> bytes (for wt)
-    # 2. skip the intermediary and just iterate through ndarray and convert to bytes
-    # Both seem to be producing the same (incorrect) output. That's something, but I'm not sure what.
+    # # With out new width and height determined, we can resize the ndarray to the new size.
+    # # TODO: add a flag to switch interpolation algorithms. more: https://stackoverflow.com/questions/48121916/numpy-resize-rescale-image
+    # resized_array = cv2.resize(array, dsize=(resize_width, resize_height), interpolation=cv2.INTER_CUBIC)
+    # # If you would like to play around with the dsize dimensions and visualize the output, so that and uncomment this next line:
+    # # rasterio.plot.show(resized_array)
 
-    # Approach 1. This is from [Reddit](https://www.reddit.com/r/synthesizers/comments/84rlal/need_help_with_a_weird_csv_to_audio_conversion/).
-    # Set up a temporary file where we can write the WAV data.
-    temp_file: tempfile._TemporaryFileWrapper[bytes] = tempfile.NamedTemporaryFile()
-    wave_data: wave.Wave_write = wave.open(temp_file.name, "wb")
+    # # There are two approaches I can think of:
+    # # 1. convert ndarray -> WAV and then WAV -> bytes (for wt)
+    # # 2. skip the intermediary and just iterate through ndarray and convert to bytes
+    # # Both seem to be producing the same (incorrect) output. That's something, but I'm not sure what.
 
-    # Define parameters for the wave file.
-    channels = 1  # 1 = mono, 2 = stereo
-    sample_width = 2
-    sample_rate = 44100
-    number_of_audio_frames = 0
-    compression_type = "NONE"
-    compression_name = "not compressed"
-    wave_data.setparams(
-        (
-            channels,
-            sample_width,
-            sample_rate,
-            number_of_audio_frames,
-            compression_type,
-            compression_name,
-        )
-    )
+    # # Approach 1. This is from [Reddit](https://www.reddit.com/r/synthesizers/comments/84rlal/need_help_with_a_weird_csv_to_audio_conversion/).
+    # # Set up a temporary file where we can write the WAV data.
+    # temp_file: tempfile._TemporaryFileWrapper[bytes] = tempfile.NamedTemporaryFile()
+    # wave_data: wave.Wave_write = wave.open(temp_file.name, "wb")
 
-    # Write each wave in the dataset's array into a temporary WAV file.
-    for waveform in resized_array:
-        wave_data.writeframes(waveform)
-    wave_data.close()
+    # # Define parameters for the wave file.
+    # channels = 1  # 1 = mono, 2 = stereo
+    # sample_width = 2
+    # sample_rate = 44100
+    # number_of_audio_frames = 0
+    # compression_type = "NONE"
+    # compression_name = "not compressed"
+    # wave_data.setparams(
+    #     (
+    #         channels,
+    #         sample_width,
+    #         sample_rate,
+    #         number_of_audio_frames,
+    #         compression_type,
+    #         compression_name,
+    #     )
+    # )
 
-    # Now that the data is in a format we know how to work with (i.e., WAV), we read the WAV file frame by frame and put those bytes into a list.
-    databuffer: list[bytes] = []
-    with wave.open(temp_file, "rb") as wav_file:
-        n_frames: int = wav_file.getnframes()
-        sample_width: int = wav_file.getsampwidth()
-        content: bytes = wav_file.readframes(n_frames * sample_width)
-        databuffer.append(content)
+    # # Write each wave in the dataset's array into a temporary WAV file.
+    # for waveform in resized_array:
+    #     wave_data.writeframes(waveform)
+    # wave_data.close()
 
-    # Approach 2
+    # # Now that the data is in a format we know how to work with (i.e., WAV), we read the WAV file frame by frame and put those bytes into a list.
     # databuffer: list[bytes] = []
-    # for current_wave in resized_array:
-    #     databuffer.append(b"".join(current_wave))
+    # with wave.open(temp_file, "rb") as wav_file:
+    #     n_frames: int = wav_file.getnframes()
+    #     sample_width: int = wav_file.getsampwidth()
+    #     content: bytes = wav_file.readframes(n_frames * sample_width)
+    #     databuffer.append(content)
 
-    rasterio.plot.show(databuffer)
-    return databuffer
+    # # Approach 2
+    # # databuffer: list[bytes] = []
+    # # for current_wave in resized_array:
+    # #     databuffer.append(b"".join(current_wave))
+
+    # rasterio.plot.show(databuffer)
+    # return databuffer
 
 
 def display_info(dataset: rasterio.io.DatasetReader) -> None:
--- src/tests/test_shift_bit_length.py
+++ src/tests/test_shift_bit_length.py
@@ -6,7 +6,7 @@ Verifies the utility function correctly finds the next greatest power of 2.
 from main import shift_bit_length
 
 
-def test_shift_bit_length():
+def test_shift_bit_length() -> None:
     assert shift_bit_length(2047) == 2048
     assert shift_bit_length(2048) == 2048
     assert shift_bit_length(2049) == 4096
